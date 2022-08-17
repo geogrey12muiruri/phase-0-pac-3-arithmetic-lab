57e0622b511a4ca8a41b307e9f8c6f37cd3d92a2
geogrey12muiruri <greggambugua@gmail.com>
2022-08-17 11:11:07 +0300

    completed the assaignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+    return a+b;
+
+
+}
+function subtract(a, b) {
+return a-b;
+}
+function multiply(a, b) {
+    return a*b;
+
+}
+function divide(a,b) {
+    return a/b;
+
+}
+let a;
+function increment(a) {
+    return a+1;
+}
+let b = 726;
+function decrement(a) {
+    return a-b;
+}
+function makeInt(n) {
+    return parseInt(a, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}
+n++;
+
+n--;
+
+add(a, b);
+subtract(a,b);
+multiply(a, b);
+divide(a, b);
